44b8773048fdf1ba190d1bb2416b6ca5ec845f92
Sang Bae <sw2.bae@gmail.com>
2020-06-01 23:25:49 -0500

    rev05_SQLpassword Protected

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 
@@ -6,25 +7,25 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "Qotkddn0925!",
+    password: process.env.password,
     database: "company"
-  });
+});
 
-  connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) {
-      console.error("error connecting: " + err.stack);
-      return;
+        console.error("error connecting: " + err.stack);
+        return;
     }
-  
+
     console.log("connected as id " + connection.threadId);
     init();
-  });
+});
 
-  function mainmenu() {
+function mainmenu() {
     return inquirer.prompt([
         {
             type: "list",
-            message: "What Would you like to do ?",
+            message: "What would you like to do ?",
             name: "main",
             choices: [
                 "View All Department",
@@ -41,39 +42,62 @@ var connection = mysql.createConnection({
         }
     ]);
 }
+const currentDepartment =[];
+
+function departmentStatus(){
+    connection.query("SELECT name FROM department", function(err,res){
+        currentDepartment.push(res);
+        console.log(res);
+    });
+}
+
+function deleteDepartmentMenu(){
+    departmentStatus();
+    console.log(currentDepartment);
+    return inquirer.prompt([{
+        type:"list",
+        message: "Which Department do you want to Delete ?",
+        name: "departmentDelete",
+        choices: currentDepartment
+    }
+]);
+}
 
 function viewDepartment() {
-    connection.query("SELECT * FROM department", function(err, res) {
-      if (err) throw err;
-      console.table(res);
-      connection.end();
+    connection.query("SELECT * FROM department", function (err, res) {
+        if (err) throw err;
+        console.table(res);
+        connection.end();
     });
-  }
-  function viewRoles() {
-    connection.query("SELECT * FROM role", function(err, res) {
-      if (err) throw err;
-      console.table(res);
-      connection.end();
+}
+function viewRoles() {
+    connection.query("SELECT * FROM role", function (err, res) {
+        if (err) throw err;
+        console.table(res);
+        connection.end();
     });
-  }
-  function viewEmployee() {
-    connection.query("SELECT * FROM employee", function(err, res) {
-      if (err) throw err;
-      console.table(res);
-      connection.end();
+}
+function viewEmployee() {
+    connection.query("SELECT * FROM employee", function (err, res) {
+        if (err) throw err;
+        console.table(res);
+        connection.end();
     });
-  }
+}
+
 
 async function init() {
     try {
         const menu = await mainmenu();
-        switch(menu.main){
+        switch (menu.main) {
             case "View All Department":
                 return viewDepartment();
             case "View All Roles":
                 return viewRoles();
             case "View All Employee":
                 return viewEmployee();
+            case "Delete Department":
+                return deleteDepartmentMenu();
         }
     }
     catch (err) {
